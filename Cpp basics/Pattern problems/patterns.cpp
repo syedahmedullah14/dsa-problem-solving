7bc6dbf034fa656156e970baf6120ed57a298aff
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-17 18:33:30 +0530

    Number patterns

--- Cpp basics/Pattern problems/patterns.cpp
+++ Cpp basics/Pattern problems/patterns.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+//numver pattern
+void nTriangle(int n) {
+
+	for(int i = 1; i<=n; i++){
+		for(int j=1; j<=i;j++){
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+
+}
+
 int main() {
 
+    //basic star pattern
     for(int i =0; i<5; i++){
         for(int j=0; j<i; j++){
             cout<< "*" ;
