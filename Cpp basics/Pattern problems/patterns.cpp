6d8ade929d820f0227429d1a9198419b8a97562b
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-17 19:26:14 +0530

    reverse space and stars medium difficulty

--- Cpp basics/Pattern problems/patterns.cpp
+++ Cpp basics/Pattern problems/patterns.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+//reverse space and stars medium difficulty
+void nStarTriangle(int n) {
+   for(int i=0; i<n; i++){
+         //space
+         for(int j=0; j<i; j++){
+             cout<<" ";
+         }
+         //star
+         for(int j=0; j<2*n-(2*i + 1); j++){
+             cout<<"*";
+         }
+         //space
+         for(int j=0; j<i; j++){
+             cout<<" ";
+         }
+         cout<< endl;
+    }
+}
+
 //space and stars medium difficulty
 void nStarTriangle(int n) {
 
