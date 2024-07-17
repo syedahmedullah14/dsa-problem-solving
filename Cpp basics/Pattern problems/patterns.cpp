82222f2faeac3e57b3f0f113ab6a8bac17a9953a
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-17 18:44:02 +0530

    reverse stars

--- Cpp basics/Pattern problems/patterns.cpp
+++ Cpp basics/Pattern problems/patterns.cpp
@@ -13,6 +13,16 @@ void nTriangle(int n) {
 
 }
 
+//reverse stars
+void seeding(int n) {
+	for(int i = n; i>0; i--){
+        for(int j=0; j<i; j++){
+            cout<< "* " ;
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
 
     //basic star pattern
