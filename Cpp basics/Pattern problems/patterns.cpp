ed7734252bf36272b16d86506ff2163d07cab864
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-18 22:14:52 +0530

    Star diamond

--- Cpp basics/Pattern problems/patterns.cpp
+++ Cpp basics/Pattern problems/patterns.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 using namespace std;
 
+
+//star diamond difficulty - medium
+void nStarDiamond(int n) {
+    // Write your code here.
+    for(int i=0; i<n; i++){
+         //space
+         for(int j=0; j<n-i-1; j++){
+             cout<<" ";
+         }
+         //star
+         for(int j=0; j<2*i+1; j++){
+             cout<<"*";
+         }
+         //space
+         for(int j=0; j<n-i-1; j++){
+             cout<<" ";
+         }
+
+         cout<< endl;
+    }
+
+    for(int i=0; i<n; i++){
+         //space
+         for(int j=0; j<i; j++){
+             cout<<" ";
+         }
+         //star
+         for(int j=0; j<2*n-(2*i + 1); j++){
+             cout<<"*";
+         }
+         //space
+         for(int j=0; j<i; j++){
+             cout<<" ";
+         }
+         cout<< endl;
+    }
+}
+
 //reverse space and stars medium difficulty
 void nStarTriangle(int n) {
    for(int i=0; i<n; i++){
