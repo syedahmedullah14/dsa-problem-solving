f44509d62687ea2e06106acb1c5377f523fe56b7
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-19 14:49:15 +0530

    binary triangle

--- Cpp basics/Pattern problems/patterns.cpp
+++ Cpp basics/Pattern problems/patterns.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+//binary triangle
+void nBinaryTriangle(int n) {
+
+    int start = 1;
+    for(int i = 0; i<n; i++){
+        if(i%2==0) start = 1;
+        else start = 0;
+        for(int j=0; j<=i; j++){
+            cout<<start <<" ";
+            start = 1 - start;
+        }
+        cout<<endl;
+    }
+}
 
 //star diamond difficulty - medium
 void nStarDiamond(int n) {
