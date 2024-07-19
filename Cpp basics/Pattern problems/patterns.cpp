f2b183bff351cb48df8b3d9c6bc298d35d6c3b8f
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-19 14:53:41 +0530

    Alpha-ramp

--- Cpp basics/Pattern problems/patterns.cpp
+++ Cpp basics/Pattern problems/patterns.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
+//aplha-ramp
+void alphaRamp(int n) {
+    // Write your code here.
+    for(int i=0; i<n; i++){
+        char ch = 'A' +i;
+        for(int j=0; j<=i; j++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 
 //reverse letter triangle
 void nLetterTriangle(int n) {
