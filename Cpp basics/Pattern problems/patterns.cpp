a1121c99a71a65ee7219ca3da2bbd8bfddd5b980
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-19 14:52:46 +0530

    Reverse letter triangle

--- Cpp basics/Pattern problems/patterns.cpp
+++ Cpp basics/Pattern problems/patterns.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//reverse letter triangle
+void nLetterTriangle(int n) {
+
+    for(int i=0; i<n; i++){
+        for(char ch = 'A'; ch <= 'A' + (n - i - 1); ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
+
 //Increasing letter traingle
 void nLetterTriangle(int n) {
 
