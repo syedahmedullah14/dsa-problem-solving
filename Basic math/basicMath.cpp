e8f9a19e853e788883e9613bcf9672f29141767d
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-22 17:23:06 +0530

    Print all divisor

--- Basic math/basicMath.cpp
+++ Basic math/basicMath.cpp
@@ -63,3 +63,21 @@ bool checkArmstrong(int n){
 	 }
 }
 
+//Print al divisor
+int* printDivisors(int n, int &size){
+
+    vector <int> ls;
+    for(int i=1; i<=sqrt(n); i++){
+        if(n%i==0){
+            ls.push_back(i); 
+
+            if((n/i) != i){
+                ls.push_back(n/i);
+            }
+        }
+
+    }
+    sort(ls.begin(), ls.end());
+    for(auto it: ls) cout<<it<<" ";
+}
+
