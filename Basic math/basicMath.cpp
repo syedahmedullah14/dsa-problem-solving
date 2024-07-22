dc79ca5d1a8e381ae7c9f96cfc3325540de2e683
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-22 17:21:08 +0530

    Palindrome

--- Basic math/basicMath.cpp
+++ Basic math/basicMath.cpp
@@ -23,3 +23,23 @@ int countDigits(int n) {
   int cnt = (int)(log10(n) + 1);
   return cnt;
 }
+
+//palindrome
+bool palindrome(int n)
+{
+
+    int original = n;
+    int revN=0;
+	while(n>0){
+		int ld = n%10;
+		n = n/10;
+		revN = (revN*10) +ld;
+
+	}
+    if(original==revN){
+        return true;
+    }	
+    else{
+        return false;
+    }
+}
