42586086778d7711f96c65fbc84386f11563e738
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-23 19:23:57 +0530

    Backtracking algorithm

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -18,3 +18,24 @@ int main(){
     print();
   return 0;
 }
+
+//backtracking
+#include<bits/stdc++.h>
+using namespace std;
+int cnt = 0;
+
+int print(n,n){
+  
+   // Base Condition.
+   if(cnt == 3)  return;
+
+   print(n-1,n);
+  cout<<"Jaser"<<endl;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    print(n,n);
+  return 0;
+}
