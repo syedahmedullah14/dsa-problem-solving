b2535f4ab7e479816e76bb95611c5a21d93e2001
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-23 16:39:40 +0530

    Recursion

---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int cnt = 0;
+
+void print(){
+   
+   // Base Condition.
+   if(cnt == 3)  return;
+   cout<<cnt<<endl;
+
+   // Count Incremented
+   cnt++;
+   print();
+
+}
+
+int main(){
+    print();
+  return 0;
+}
