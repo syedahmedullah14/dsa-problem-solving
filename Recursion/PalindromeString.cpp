c446306129323c3d9c44b787fc021ce20eefc76b
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-24 19:52:31 +0530

    Check if a string is a palindrome or not

---
+++ Recursion/PalindromeString.cpp
@@ -0,0 +1,15 @@
+bool print(int i, string &s){
+  
+   if(i>=s.size() /2) return true;
+
+    if(s[i] != s[s.size()-i-1]) return false;
+    return print(i+1, s);
+
+}
+
+int main(){
+   string s = "madam";
+    cout<<print(0, s);
+
+  return 0;
+}
