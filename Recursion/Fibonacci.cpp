596a5a3efb55b4f52bc64a5e62046258dbf4c4d7
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-25 18:45:01 +0530

    Fibonacci using multiple recursions

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int f(int n){
+    if(n<=1) return n;
+    int last = f(n-1);
+    int slast = f(n-2);
+
+    return last + slast;
+}
+
+int main() {
+    cout << f(4);
+    return 0;
+}
