be039162b88dafe7e4b903cd990ab63a2e2bf7e1
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-25 21:43:14 +0530

    Hashing with strings using ASCII values

---
+++ Hashing/HashArrayString.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+
+    //precompute
+    int hash[256] = {0}; //for all ASCII characters 
+    for(int i = 0; i<n; i++){
+        hash[s[i]]++;
+        //hash[s[i]-'a'] // to compute only lower character
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        //fetch
+        cout<<hash[c]<<endl;
+    }
+}
