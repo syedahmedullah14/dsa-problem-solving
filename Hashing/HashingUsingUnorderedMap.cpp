a8ecbe796b74c912c0a1f841cbdaaa21dcaa0568
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-29 21:42:12 +0530

    Another method

--- Hashing/HashingUsingUnorderedMap.cpp
+++ Hashing/HashingUsingUnorderedMap.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 
+//hashing with unordered map to achive O(1) time complexity... as it rarely goes to O(n) in the worst case
 int main(){
     int n;
     cin>>n;
@@ -7,6 +8,17 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
+
+    //another method
+    // unordered_map<int, int> mpp;
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // for(int i=0; i<n; i++){
+    //     cin>>arr[i];
+    //     mpp[arr[i]]++;
+
+    // }
 
     //pre-compute
     unordered_map<int, int> mpp;
