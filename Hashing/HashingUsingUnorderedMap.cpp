3f8cfc1217d5f398117e3b87bd332efe6ca6cb3c
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-29 21:39:54 +0530

    Unordered maps

---
+++ Hashing/HashingUsingUnorderedMap.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    //pre-compute
+    unordered_map<int, int> mpp;
+    for(int i=0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+
+    //iterate in the map
+    for(auto it: app){
+        cout<< it.first <<"->" << it.second << endl;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        //fetch
+    }
+}
