23b0cc8112dcb2171f0e72496af5dfaed767fa39
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-30 21:21:28 +0530

    Sorting techniques -> SelectionSort

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -1,35 +1,28 @@
-#include<bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
-void selection_sort(int arr[], int n) {
-  // selection sort
-  for (int i = 0; i < n - 1; i++) {
-    int mini = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[mini]) {
-        mini = j;
-      }
+void selection_sort(int arr[], int n){
+    for(int i = 0; i<n-2; i++){
+        int min = i;
+        for(int j = i; j<=n-1; j++ ){
+            if(arr[j]<arr[i]){
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
     }
-    int temp = arr[mini];
-    arr[mini] = arr[i];
-    arr[i] = temp;
-  }
-
-  cout << "After selection sort: " << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-
 }
+
 int main() {
-  int arr[] = {13,46,24,52,20,9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-   cout << "Before selection sort: " << "\n";
-   for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-  selection_sort(arr, n);
-  return 0;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+    selection_sort(arr, n);
+    for(int i =0; i<n; i++){
+        cout<<arr[i]<< " ";
+    }
+    return 0;
 }
